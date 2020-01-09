1d96d400cd3a8300a1c3a8c2b6411066fd09abe7
Dave Franz <davidlawrencefranz@gmail.com>
2020-01-08 21:26:59 -0800

    completed ImageGallery component and included dependencies

--- client/components/ImageGallery.jsx
+++ client/components/ImageGallery.jsx
@@ -3,10 +3,23 @@ import { render } from "react-dom";
 import Gallery from "react-photo-gallery";
 import Carousel, { Modal, ModalGateway } from "react-images";
 import { photos } from "../src/photos";
+import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 
+// STYLE FOR HORIZONTAL SCROLL
+// style = "display: flex; flex-flow: row; overflow-x: auto"
+const useStyles = makeStyles({
+  horizontal: {
+    display: 'flex', 
+    flexFlow: 'row',
+    overflowX: 'auto'
+  },
+});
 // Can update to toggle photos - potentially add to a different stop
 // Can update to rearrange photos with drag and drop
 const ImageGallery = props => {
+  const classes = useStyles();
+
   const [currentImage, setCurrentImage] = useState(0);
   const [viewerIsOpen, setViewerIsOpen] = useState(false);
 
@@ -22,7 +35,7 @@ const ImageGallery = props => {
 
   return (
     <div>
-      <Gallery photos={photos} onClick={openLightbox} />
+      <Gallery photos={photos} onClick={openLightbox} className={classes.horizontal}/>
       <ModalGateway>
         {viewerIsOpen ? (
           <Modal onClose={closeLightbox}>
@@ -42,3 +55,8 @@ const ImageGallery = props => {
 }
 
 export default ImageGallery;
+
+// export default function Hook() {
+//   const classes = useStyles();
+//   return <Button className={classes.root}>Hook</Button>;
+// }
